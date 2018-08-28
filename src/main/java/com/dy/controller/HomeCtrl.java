7021d676c4f7132cd386a75f62026a04b4a7b13c
chenyufeng <411610370@qq.com>
2018-08-28 21:51:33 +0800

    Initial commit

---
+++ src/main/java/com/dy/controller/HomeCtrl.java
@@ -0,0 +1,13 @@
+package com.dy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeCtrl {
+    @RequestMapping(value = "/test.do",method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
