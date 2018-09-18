03fb7f454a2eff51205b33727934447face968ce
aygrhn1991 <411610370@qq.com>
2018-09-18 08:31:17 +0800

    0

--- src/main/java/com/dy/controller/OAuthCtrl.java
+++ src/main/java/com/dy/controller/OAuthCtrl.java
@@ -1,6 +1,7 @@
 package com.dy.controller;
 
 import com.dy.model.wx.OAuthUserAccessToken;
+import com.dy.model.wx.OAuthUserInfo;
 import com.dy.model.wx.UserInfoModel;
 import com.dy.util.Global;
 import com.dy.util.HttpUtil;
@@ -53,12 +54,46 @@ public class OAuthCtrl {
     public String auth(HttpServletRequest request) throws UnsupportedEncodingException {
         String baseUrl = HttpUtil.getBaseUrlWithoutPort(request);
         String encodeUrl = URLEncoder.encode(baseUrl + "/oauth/getcode", "utf-8");
-        String url = String.format("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=%s&state=%s#wechat_redirect", this.global.wxAppid, encodeUrl, "snsapi_base", "dy");
+        String url = String.format("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=%s&state=%s#wechat_redirect", this.global.wxAppid, encodeUrl, "snsapi_userinfo", "dy");
         return "redirect:" + url;
     }
 
     @RequestMapping(value = "/getcode", method = RequestMethod.GET)
     public String getcode(HttpServletRequest request, HttpServletResponse response) {
+        String state = request.getParameter("state");
+        String code = request.getParameter("code");
+        String url = String.format("https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code", this.global.wxAppid, this.global.wxAppsecret, code);
+        String rsp = HttpUtil.Get(url);
+        Gson gson = new Gson();
+        OAuthUserAccessToken oAuthUserAccessToken = gson.fromJson(rsp, OAuthUserAccessToken.class);
+        String sql = "select * from t_user where w_openid=?";
+        List<Map<String, Object>> userList = this.jdbcTemplate.queryForList(sql, new Object[]{oAuthUserAccessToken.openid});
+        if (userList.size() == 1) {
+            Cookie cookie = new Cookie("userid", userList.get(0).get("t_id").toString());
+            cookie.setDomain(request.getServerName());
+            cookie.setPath("/");
+            response.addCookie(cookie);
+            return "redirect:/home/index";
+        } else {
+
+            url = String.format("https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN", oAuthUserAccessToken.access_token, oAuthUserAccessToken.openid);
+            rsp = HttpUtil.Get(url);
+            OAuthUserInfo oAuthUserInfo = gson.fromJson(rsp, OAuthUserInfo.class);
+            sql = "insert into t_user(w_openid,w_nickname,w_sex,w_province,w_city,w_country,w_headimgurl,t_time) values (?,?,?,?,?,?,?,?)";
+            int count = this.jdbcTemplate.update(sql, new Object[]{oAuthUserInfo.openid,
+                    oAuthUserInfo.nickname.replaceAll("[\ue000-\uefff]", ""),
+                    oAuthUserInfo.sex,
+                    oAuthUserInfo.province,
+                    oAuthUserInfo.city,
+                    oAuthUserInfo.country,
+                    oAuthUserInfo.headimgurl,
+                    new Date().getTime()});
+            return "redirect:/oauth/requestcode";
+        }
+    }
+
+    @RequestMapping(value = "/getcode2", method = RequestMethod.GET)
+    public String getcode2(HttpServletRequest request, HttpServletResponse response) {
         String state = request.getParameter("state");
         String code = request.getParameter("code");
         String url = String.format("https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code", this.global.wxAppid, this.global.wxAppsecret, code);
