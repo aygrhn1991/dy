f727dd2f885200b56bded122c0db24e4aec2efdc
chenyufeng <411610370@qq.com>
2018-10-10 20:32:00 +0800

    1

--- src/main/java/com/dy/controller/HomeCtrl.java
+++ src/main/java/com/dy/controller/HomeCtrl.java
@@ -182,9 +182,11 @@ public class HomeCtrl {
             list = this.jdbcTemplate.queryForList(sql);
         }
         //处理搜索次数
-        for (Map<String, Object> m : list) {
-            sql = "update t_article set t_search=t_search+1 where t_id=?";
-            this.jdbcTemplate.update(sql, new Object[]{m.get("t_id")});
+        if (keyword != null || !keyword.equals("")) {
+            for (Map<String, Object> m : list) {
+                sql = "update t_article set t_search=t_search+1 where t_id=?";
+                this.jdbcTemplate.update(sql, new Object[]{m.get("t_id")});
+            }
         }
         return list;
     }
