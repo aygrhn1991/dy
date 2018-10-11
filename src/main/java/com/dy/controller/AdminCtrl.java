67c6b78a04a766600df33b33c7dfeb2c8327b282
aygrhn1991 <411610370@qq.com>
2018-10-11 16:13:32 +0800

    0

--- src/main/java/com/dy/controller/AdminCtrl.java
+++ src/main/java/com/dy/controller/AdminCtrl.java
@@ -185,7 +185,7 @@ public class AdminCtrl {
     @ResponseBody
     public boolean addarticle(@RequestBody Article article) {
         String sql = "insert into t_article(t_type_id, t_title, t_author, t_time, t_cover, t_content, t_url, t_mode, t_scan, t_sort, t_top, t_scan_origin, t_search) values (?,?,?,?,?,?,?,?,0,0,0,0,0)";
-        int count = this.jdbcTemplate.update(sql, new Object[]{article.t_type_id, article.t_title, article.t_author, article.t_time, article.t_cover, article.t_content, article.t_url, (article.t_url == null || article.t_url.equals("")) ? 0 : 1});
+        int count = this.jdbcTemplate.update(sql, new Object[]{article.t_type_id, article.t_title, article.t_author, article.t_time, article.t_cover, article.t_content, article.t_url, (article.t_url == null || article.t_url.equals("")) ? 1 : 0});
         return count == 1;
     }
 
@@ -193,7 +193,7 @@ public class AdminCtrl {
     @ResponseBody
     public boolean editarticle(@RequestBody Article article) {
         String sql = "update t_article set t_type_id=?,t_title=?,t_author=?,t_time=?,t_cover=?,t_content=?,t_url=?,t_mode=? where t_id=?";
-        int count = this.jdbcTemplate.update(sql, new Object[]{article.t_type_id, article.t_title, article.t_author, article.t_time, article.t_cover, article.t_content, article.t_url, (article.t_url == null || article.t_url.equals("")) ? 0 : 1, article.t_id});
+        int count = this.jdbcTemplate.update(sql, new Object[]{article.t_type_id, article.t_title, article.t_author, article.t_time, article.t_cover, article.t_content, article.t_url, (article.t_url == null || article.t_url.equals("")) ? 1 : 0, article.t_id});
         return count == 1;
     }
 
